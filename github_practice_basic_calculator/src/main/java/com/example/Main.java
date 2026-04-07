0b46990c16afcde80779743b688cd1d724b17d8a
Hanoz Daruwalla <Hanozdaru@outlook.com>
2026-04-07 18:11:55 +0100

    simple ui complete
    added asking for calc type and passing throgugh to functions

--- github_practice_basic_calculator/src/main/java/com/example/Main.java
+++ github_practice_basic_calculator/src/main/java/com/example/Main.java
@@ -1,8 +1,39 @@
 package com.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double result = 0.99;
+
+        Scanner user_input_scanner = new Scanner(System.in);
+        System.out.println(" ----------- Welcome To Calculator Program -----------");
+        System.out.print("Enter first number: ");
+        double num1 = user_input_scanner.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = user_input_scanner.nextDouble();
+        System.out.println("What Calculation You Want To Do? ('+' '-' '*' '/')");
+        char operation = user_input_scanner.next().charAt(0);
+
+        switch(operation){
+            case '+':
+                result = addition(num1, num2);
+                break;
+            case '-':
+                result = subtraction(num1, num2);
+                break;
+            case '*':
+                result = multiplication(num1,num2);
+                break;
+            case '/':
+                result = division(num1, num2);
+                break;
+            default:                
+                System.out.println("Invalid Operation");
+        }
+
+        System.out.println(num1 + " " + operation +  " " + num2 + " =" + result);
+        System.out.println(" Result = " + result);  
     }
     
     private static double addition(double a, double b) {
@@ -11,10 +42,12 @@ public class Main {
 
     private static double subtraction(double a, double b) {
         return a + b;
+        //change later
     }
 
     private static double multiplication(double a, double b) {
         return a + b;
+        //change later    
     }
 
     private static double division(double a, double b) {
@@ -22,5 +55,6 @@ public class Main {
             throw new IllegalArgumentException("Cannot divide by zero");
         }
         return a + b;
+        //change later
     }
 }
