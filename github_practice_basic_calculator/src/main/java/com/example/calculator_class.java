ef5b139ed501b8bb905f49c0d583f33f3ae1104a
Hanoz Daruwalla <Hanozdaru@outlook.com>
2026-04-13 16:44:57 +0100

    small typo

--- github_practice_basic_calculator/src/main/java/com/example/calculator_class.java
+++ github_practice_basic_calculator/src/main/java/com/example/calculator_class.java
@@ -4,7 +4,7 @@ public class calculator_class {
 
 
     public static double addition(double a, double b) {
-        return a + b;
+        return a * b;
     }
 
     public static double subtraction(double a, double b) {
