15c7c405a8c8f37c85296c42e1d0836d914411b6
Hanoz Daruwalla <Hanozdaru@outlook.com>
2026-05-08 17:12:05 +0100

    fix UI_Get_Numbers to fetch old answer and use in calculaitons

--- github_practice_basic_calculator/src/main/java/com/example/Main.java
+++ github_practice_basic_calculator/src/main/java/com/example/Main.java
@@ -19,7 +19,9 @@ public class Main {
         Scanner user_input_scanner = new Scanner(System.in);
 
         System.out.print("Enter First number (or 'Answer' for previous answer): ");
-        num1 = UI_Get_Numbers(calculator, user_input_scanner);      
+        
+        num1 = UI_Get_Numbers(calculator, user_input_scanner);
+              
         
         System.out.print("Enter Second number (or 'Answer' for previous answer): ");
         num2 = UI_Get_Numbers(calculator, user_input_scanner);
@@ -55,11 +57,20 @@ public class Main {
 
     private static double UI_Get_Numbers(calculator_class calculator, Scanner user_input_scanner) {
         double temp_number = 0.00;
+        String user_input_string = "Undefined";
+            
         try{
             temp_number = user_input_scanner.nextDouble();
+            System.err.println("Temp Number set to: " + temp_number);
         }catch(InputMismatchException e){
-            if(user_input_scanner.nextLine().equals("Answer") || user_input_scanner.nextLine().equals("answer")){
+
+            //next getsd what was last written
+            user_input_string = user_input_scanner.next();
+
+            if(user_input_string.equals("Answer") || user_input_string.equals("answer")){
                 temp_number = calculator.getPrevious_answer();
+                System.out.println("Ans (" + temp_number + ")");
+
             }else{
                 System.out.println("Invalid Input, Please Try Again");
                 //MAKE VALID REDO CYCLE !!!!!!!!!                
@@ -82,7 +93,8 @@ public class Main {
         Scanner user_input_for_continuation = new Scanner(System.in);
         System.out.println("Do You Want To Continue? (Y/N)");
         char user_response = user_input_for_continuation.next().charAt(0);
-       if(user_response == 'Y' || user_response == 'y'){
+        user_input_for_continuation.nextLine(); //resets scanner       
+        if(user_response == 'Y' || user_response == 'y'){
             System.out.println("\n\n");
             System.out.println(" ----------- Carrying On Calculator Program -----------");
             UI_Get_Calculation(calculator);
